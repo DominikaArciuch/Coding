fad1a29a520a0b2fef0b804fef8d803ca584bec1
DominikaArciuch <arcdom703@gmail.com>
2024-06-10 22:46:10 +0200

    fix: comment print

--- backend/fun.py
+++ backend/fun.py
@@ -11,8 +11,8 @@ def find_unique(list_el):
 # this solution use function Counter which I briefly mention during interview
 def find_unique2(list_el):
     list_el_count = collections.Counter(list_el)
-    list_unique = filter(lambda x: x[1] == 1, list_el_count.items())
-    return [i[0] for i in list_unique]
+    list_unique = list(filter(lambda x: x[1] == 1, list_el_count.items()))
+    return list_unique[0][0]
 
 
-print(find_unique2([int(i) for i in input().split()]))
+# print(find_unique2([int(i) for i in input().split()]))
--- backend/test_fun.py
+++ backend/test_fun.py
@@ -12,9 +12,8 @@ def test_find_unique(list_el, result):
 
 
 @pytest.mark.parametrize("list_el, result", [
-    ([1, 2, 2, 3], [1, 3]),
-    ([1, 1, 2, 3, 3, 1, 4, 7, 9], [2, 4, 7, 9]),
-    ([4, 2, 4, 0, 1, 1], [2, 0])
+    ([1, 1, 2, 2, 3], 3),
+    ([1, 1, 2, 2, 3, 3, 1, 4], 4)
 
 ])
 def test_find_unique2(list_el, result):
